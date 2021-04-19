e75663fcce82595937749e24dfc69002f1c3b5b5
himupeki236 <usodatoitteyojoe@gmail.com>
2021-04-19 15:39:57 +0900

    文言修正

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,6 +4,9 @@ class Artist < ApplicationRecord
   with_options presence: true do
     validates :image
     validates :name, length: { maximum: 30 }
+    validates :genre
+    validates :start_year, numericality: { only_integer: true }, length: { is: 4 }
   end
   validates :profile,  length: { maximum: 1000 }
+  validates :end_year, numericality: { only_integer: true }, length: { is: 4 }
 end
